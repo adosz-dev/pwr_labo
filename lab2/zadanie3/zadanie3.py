2f2152a4b79a2538a3dcd4c67bb564d10dff67f3
Adam Latos-Wa??ny <adamlw06@protonmail.com>
2025-11-03 13:30:16 +0100

    zadanie 3 lab 2 poprawione w pythonie

--- lab2/zadanie3/zadanie3.py
+++ lab2/zadanie3/zadanie3.py
@@ -1,36 +1,39 @@
-def wypisz(d, potega):
-	if potega:
-		if potega > 1:
-			print(f"{d}^{potega}", end="")
-		else:
-			print(f"{d}", end="")
+def wypisz(dzielniki, n):
+	elementy = []
+	print(f"{n}=",end="")
+	for d, potega in dzielniki.items():
+		if potega:
+			if potega > 1:
+				elementy.append(f"{d}^{potega}")
+			else:
+				elementy.append(f"{d}")
+	print("*".join(elementy))
 
 
-def rozklad(n):
+def rozklad(n, dzielniki):
 	d = 3
 	dd = 9
 	potega = 0
+
 	while n%2 == 0:
 		potega += 1
 		n = n // 2
-	wypisz(2, potega)
+	dzielniki[2] = potega
 	
 	while n >= dd:
-		print("*", end="")
 		potega = 0
 		while n%d == 0:
 			n = n // d
 			potega += 1
-		wypisz(d, potega)
+		dzielniki[d] = potega
 
 		dd = dd + 4*d +4
 		d = d + 2
-	if n > 1:	
-		print("*", end="")
-		wypisz(n, 1)
-
+	if n > 1:
+		dzielniki[n] = 1
 
 if __name__=="__main__":
 	n = int(input("Podaj liczbę: "))
-	print(f"{n}=",end="")
-	rozklad(n)
+	dzielniki = {}
+	rozklad(n, dzielniki)
+	wypisz(dzielniki, n)
