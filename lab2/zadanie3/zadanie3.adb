9efd70cb7d636c6310a713381eafee6c6eeb2226
Adam Latos-Wa??ny <adamlw06@protonmail.com>
2025-11-03 17:04:43 +0100

    zad 3 w adzie lab2

---
+++ lab2/zadanie3/zadanie3.adb
@@ -0,0 +1,67 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Unchecked_Deallocation;
+
+procedure zadanie3 is
+	Liczba: Integer;
+  Type Tablica is array (Positive range <>) of Integer;
+  Type Tablica_Ptr is access Tablica;
+  procedure Free is
+    new Standard.Ada.Unchecked_Deallocation(Tablica, Tablica_Ptr);
+  tab: Tablica_Ptr;
+  procedure Wypisz(Liczba: Integer; Tab: Tablica_Ptr) is
+    Pierwszy: Boolean := True;
+  begin
+    Put(Liczba'Image & " =");
+    for I in 2 .. Liczba loop
+      if Tab(I)>0 then
+        if not Pierwszy then
+          Put("*");
+        else
+          Pierwszy := False;
+        end if;
+        
+        if Tab(I)>1 then
+          Put(I'Image & "^" & Tab(I)'Image);
+        else
+          Put(I'Image);
+        end if;
+      end if;
+    end loop;
+  end Wypisz;
+	procedure Rozklad(Liczba: Integer; Tab: Tablica_Ptr) is
+    D: Integer := 3;
+    DD : Integer := 9;
+    N : Integer := Liczba;
+  begin
+    Tab(2) := 0;
+    while n mod 2 = 0 loop
+      Tab(2) := Tab(2) + 1;
+      N := N / 2; 
+    end loop;
+
+    while n >= dd loop
+      Tab(d) := 0;
+      while n mod d = 0 loop
+        N := N / d;
+        Tab(2) := Tab(2) + 1;
+      end loop;
+      dd := dd + 4*d + 4;
+      d := d + 2;
+    end loop;
+
+    if n > 1 then
+      Tab(n) := 1;
+    end if;
+  end Rozklad;
+  begin
+    Put("Podaj liczbę: ");
+    Get(Liczba);
+    Tab := New Tablica(2 .. Liczba);
+    for I in 2 .. Liczba loop
+      Tab(I) := 0;
+    end loop;
+    Rozklad(Liczba, Tab);
+    Wypisz(Liczba, Tab);
+    Free(Tab);
+  end zadanie3;
