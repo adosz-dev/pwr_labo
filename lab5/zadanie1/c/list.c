ed43aa5153cf64f2bcbeeb9ef0e9465cae1e8aa1
Adam Latos-Ważny <adamlw06@protonmail.com>
2025-12-14 14:43:56 +0100

    zadanie 1 lab5 c

--- lab5/zadanie1/c/list.c
+++ lab5/zadanie1/c/list.c
@@ -3,17 +3,85 @@
 #include "list.h"
 
 bool is_empty(list l){
-  return l -> first == NULL ;
+  return l -> first == NULL;
 }
 
-int pop(list l ){
+int pop(list l){
   node_ptr n = l -> first;
   int e = n -> elem;
   l -> first = l -> first -> next;
   if (l -> first == NULL) // last element
     l -> last = NULL;
   free(n);
+  l -> length --;
   return e;
+}
+
+void clean(list l){
+  while (!is_empty(l)){
+    pop(l);
+  }
+}
+
+int get(list l, int i){
+  node_ptr n = l -> first;
+  for (int j=2; j<=i; j++){
+    n = n -> next;
+  }
+  int e = n -> elem;
+  return e;
+}
+
+void put(list l, int i, int e){
+  node_ptr n= l -> first;
+  for (int j=2; j<=i; j++){
+    n = n -> next;
+  }
+  n -> elem = e;
+}
+
+void delete(list l, int i){
+  if (i == 1) {
+    pop(l);
+  }
+  else {
+    node_ptr n = l -> first;
+    int len = length(l);
+    
+    for (int j=2; j<i; j++){
+      n = n -> next;
+    }
+    node_ptr m = n -> next;
+    if (i == len){
+      n -> next = NULL;
+      l -> last = n;
+    }
+    else {
+      n -> next = m -> next;
+    }
+    free(m);
+    l -> length --;
+  }
+}
+
+void insert(list l, int i, int e){
+  if (i == 1){
+    push(l, e);
+  }
+  else if (length(l)+1 == i){
+    append(l, e);
+  }
+  else {
+    node_ptr n = l -> first;
+    for (int j=2; j<i; j++){
+      n = n -> next;
+    }
+    node_ptr c = malloc(sizeof(node));
+    c -> next = n -> next;
+    n -> next = c;
+    c -> elem = e;
+    l -> length ++;
+  }
 }
 
 void push(list l, int e){
@@ -23,6 +91,7 @@ void push(list l, int e){
   l -> first = n;
   if (l -> last == NULL) // first element
     l -> last = n;
+  l -> length ++;
 }
 
 void append(list l , int e) {
@@ -35,6 +104,7 @@ void append(list l , int e) {
     l -> last -> next = n;
   }
   l -> last = n;
+  l -> length ++;
 }
 void print(list l ){
   node_ptr n = l -> first;
@@ -46,12 +116,7 @@ void print(list l ){
 }
 
 int length (list l){
-  int i = 0;
-  node_ptr n = l -> first;
-  while (n != NULL){
-    i = i + 1;
-    n = n -> next;
-  }
-  return i;
+  int len = l -> length;
+  return len;
 }
 
--- lab5/zadanie1/c/listtest.c
+++ lab5/zadanie1/c/listtest.c
@@ -7,9 +7,11 @@
 int main(){
   char command [20];
   bool cont = true;
-  int r;
+  int r, e;
   list l = malloc(sizeof(list_t));
-  l -> first = l -> last = NULL ;
+  l -> first = NULL;
+  l -> last = NULL;
+  l -> length = 0;
   while (cont){
     printf("Command: ");
     scanf("%s", command);
@@ -45,13 +47,63 @@ int main(){
     else if (!strcmp(command, "exit")){
       cont = false;
     }
+    else if(!strcmp(command, "put")){
+      printf("Value i: ");
+      scanf("%d", &r);
+      printf("Value e: ");
+      scanf("%d", &e);
+      if (!is_empty(l) && length(l)>=r){
+        put(l, r, e);
+        printf("Result: OK\n");
+      }
+      else {
+        printf("Error - Element Doesnt Exist!\n");
+      }
+    }
+    else if(!strcmp(command, "get")){
+      printf("Value: ");
+      scanf("%d", &r);
+      if (!is_empty(l) && length(l)>=r){
+        r = get(l, r);
+        printf("Result: %d\n", r);
+      }
+      else {
+        printf("Error - Element Doesnt Exist!\n");
+      }
+    }
+    else if(!strcmp(command, "insert")){
+      printf("Value i: ");
+      scanf("%d", &r);
+      printf("Value e: ");
+      scanf("%d", &e);
+      if ((!is_empty(l) && length(l)+1 >= r) || (is_empty(l) && r==1)){
+        insert(l, r, e);
+        printf("Result: OK\n");
+      }
+      else {
+        printf("Error - Out Of Bounds!\n");
+      }
+    }
+    else if(!strcmp(command, "delete")){
+      printf("Value: ");
+      scanf("%d", &r);
+      if (!is_empty(l) && length(l)>=r){
+        delete(l, r);
+        printf("Result: OK\n");
+      }
+      else {
+        printf("Error - Element Doesnt Exist!\n");
+      }
+    }
+    else if (!strcmp(command, "clean")){
+      clean(l);
+      printf("Result: OK\n");
+    }
     else {
       printf("Unknown command!\n");
     }
   }
-  while(!is_empty(l)){
-    pop(l);
-  }
+  clean(l);
   free(l);
   return 0;
 }
